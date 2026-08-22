c62da5e294b79595fef49940111b2ce5ec7464ad
Qbit Aaaaayush <bhupeshgamedev8777@gmail.com>
2026-08-23 01:43:42 +0530

    added lens distortion

--- fpp-controller-project/addons/player/UI/motion_visual_effect_manager.gd
+++ fpp-controller-project/addons/player/UI/motion_visual_effect_manager.gd
@@ -21,12 +21,19 @@ class_name MotionVisualEffectManager
 @export var max_blur_power : float = 0.012
 @export var rblur_rect : ColorRect
 
+@export_group("lens distortion params")
+@export var min_zoom : float = 20.0
+@export var max_zoom : float = 170.0
+@export var lens_distortion_rct : ColorRect
+
+
 @export_category("juice vars")
 @export var smoothing_sped : float = 8.0
 
 var current_vg_opacitu : float
 var current_vg_intensity : float
 var current_rblur_power : float
+var current_lens_zoom : float
 
 
 # Called when the node enters the scene tree for the first time.
@@ -42,13 +49,15 @@ func _process(delta: float) -> void:
 	var desired_rblur : float = clamp(remap(speed , min_speed , max_speed , min_blur_power , max_blur_power) , min_blur_power , max_blur_power)
 	var desired_vg_intensity : float  = clamp(remap(speed , min_speed , max_speed , min_vgIntensity , max_vgIntensity ) , min_vgIntensity , max_vgIntensity)
 	var dsired_vg_opacity : float = clamp(remap(speed , min_speed , max_speed , min_vg_opacity , max_vgIntensity ) , min_vg_opacity , max_vg_opacity)
+	var desired_lens_zoom : float = clamp(remap(speed , min_speed , (max_speed*1.1) , min_zoom , max_zoom) , min_zoom , max_zoom)
 	
 	current_rblur_power = lerp(current_rblur_power , desired_rblur , smoothing_sped*delta)
 	current_vg_intensity = lerp(current_vg_intensity , desired_vg_intensity , smoothing_sped*delta)
 	current_vg_opacitu = lerp(current_vg_opacitu , dsired_vg_opacity , smoothing_sped*delta)
+	current_lens_zoom = lerp(current_lens_zoom , desired_lens_zoom , smoothing_sped*delta)
 	
 	
 	rblur_rect.material.set_shader_parameter("blur_power" , current_rblur_power)
 	vg_rect.material.set_shader_parameter("vignette_intensity" , current_vg_intensity)
 	vg_rect.material.set_shader_parameter("vignette_opacity" , current_vg_opacitu)
-	
+	lens_distortion_rct.material.set_shader_parameter("zoom" , current_lens_zoom)
